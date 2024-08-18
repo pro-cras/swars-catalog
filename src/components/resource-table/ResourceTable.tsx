ab8b0c24d6813b9ff325137f54ae0b00d0874c3a
yishai <yishai.pro@gmail.com>
2024-08-18 12:32:07 +0300

    extract logic to custom hook

--- src/components/resource-table/ResourceTable.tsx
+++ src/components/resource-table/ResourceTable.tsx
@@ -1,99 +1,36 @@
 import { Button } from "../button/Button";
-import { useSearchCategory } from "../../api/search-category";
 import { DataTable } from "./data-table";
 import { Resource } from "../../api/api";
-import { useCallback, useMemo, useState } from "react";
+import { useCallback, useState } from "react";
 import { PeopleDialog } from "../PeopleDialog/PeopleDialog";
-import { useSearchTermParam } from "../../utils/use-search-term-param";
+import { useSearchWithLocalChanges } from "../../utils/use-search-with-local-changes";
 
-const CUSTOM_ID_PREFIX = "custom/";
-function isCustomId(id: string): boolean {
-  return id.startsWith(CUSTOM_ID_PREFIX);
-}
+type Data = Resource<"people">;
+
 export function ResourceTable() {
-  const searchTerm = useSearchTermParam();
-  const query = useSearchCategory({
-    query: searchTerm ?? "",
-    category: "people",
-  });
-  type Data = Resource<"people">;
   const [selectedResource, selectResource] = useState<"new" | Data | null>(
     null,
   );
-  const [resourceChanges, setResourceChanges] = useState<
-    Map<string, Data | null>
-  >(new Map());
-  const [customId, setCustomId] = useState<number>(0);
-  const saveResource = useCallback(
-    (
-      args:
-        | { url: null; data: Omit<Data, "url" | "category"> } // create
-        | { url: string; data: Partial<Data> } // update
-        | { url: string; data: null }, // delete
-    ) => {
-      setResourceChanges((prev) => {
-        const next = new Map(prev);
-        if (args.url === null) {
-          // create
-          const id = customId.toString();
-          setCustomId((id) => id + 1);
-          next.set(id, {
-            ...args.data,
-            url: CUSTOM_ID_PREFIX + id,
-            category: "people",
-          });
-        } else if (args.data === null) {
-          // delete
-          next.set(args.url, null);
-        } else {
-          // update
-          next.set(args.url, { ...prev.get(args.url)!, ...args.data });
-        }
-        return next;
-      });
-    },
-    [customId],
-  );
-
+  const closeDialog = useCallback(() => {
+    selectResource(null);
+  }, []);
   const editResource = useCallback((resource: Data) => {
     selectResource(resource);
   }, []);
   const createNewResource = useCallback(() => {
     selectResource("new");
   }, []);
-  const localData = useMemo(() => {
-    return [
-      ...(query.data?.results ?? []),
-      ...[...resourceChanges.values()]
-        .filter((item) => !!item)
-        .filter((item) => isCustomId(item.url)),
-    ]
-      .map((resource) => {
-        const savedResourceChanges = resourceChanges.get(resource.url);
-        function resolveResourceChanges() {
-          switch (savedResourceChanges) {
-            case undefined: // no changes
-              return resource;
-            case null: // deleted
-              return null;
-            default: // updated
-              return { ...resource, ...savedResourceChanges };
-          }
-        }
-        const newValue = resolveResourceChanges();
-        return newValue;
-      })
-      .filter((resource) => !!resource);
-  }, [resourceChanges, query.data?.results]);
+
+  const { query, save } = useSearchWithLocalChanges("people");
 
   function renderContent() {
     if (query.status === "pending") {
       return <p>Loading...</p>;
     }
     if (query.status === "error") {
-      return <p>Error: {query.error.message}</p>;
+      return <p>Error: {query.error?.message}</p>;
     }
-    if (localData.length === 0) {
+    if (query.data.length === 0) {
       return <p>No results found</p>;
     }
 
@@ -103,17 +40,17 @@ export function ResourceTable() {
           <Button onClick={createNewResource}>Create</Button>
         </div>
         <DataTable
-          data={localData}
-          onDelete={(url) => saveResource({ url, data: null })}
+          data={query.data}
+          onDelete={(url) => save({ url, data: null })}
           onEdit={editResource}
         />
         <PeopleDialog
-          onClose={() => selectResource(null)}
+          onClose={closeDialog}
           onSubmit={(formData: Data) => {
             if (selectedResource === "new") {
-              saveResource({ url: null, data: formData });
+              save({ url: null, data: formData });
             } else if (selectedResource !== null) {
-              saveResource({ url: selectedResource.url, data: formData });
+              save({ url: selectedResource.url, data: formData });
             }
           }}
           person={selectedResource}
--- src/utils/use-search-with-local-changes.ts
+++ src/utils/use-search-with-local-changes.ts
@@ -1,34 +1,92 @@
 import { useCallback, useMemo, useState } from "react";
 import { Category, Resource } from "../api/api";
 import { useSearchCategory } from "../api/search-category";
+import { useSearchTermParam } from "./use-search-term-param";
 
-export function useSearchWithLocalChanges<C extends Category>(
-  category: C,
-  searchTerm: string,
-) {
+const CUSTOM_ID_PREFIX = "custom/";
+function isCustomId(id: string): boolean {
+  return id.startsWith(CUSTOM_ID_PREFIX);
+}
+
+export function useSearchWithLocalChanges<C extends Category>(category: C) {
+  const searchTerm = useSearchTermParam();
   const query = useSearchCategory({
     query: searchTerm ?? "",
-    category: "people",
+    category,
   });
 
+  type Data = Resource<"people">;
+
   const [resourceChanges, setResourceChanges] = useState<
-    Map<string, Resource<C> | null>
+    Map<string, Data | null>
   >(new Map());
-
   const [customId, setCustomId] = useState<number>(0);
+  const saveResource = useCallback(
+    (
+      args:
+        | { url: null; data: Omit<Data, "url" | "category"> } // create
+        | { url: string; data: Partial<Data> } // update
+        | { url: string; data: null }, // delete
+    ) => {
+      setResourceChanges((prev) => {
+        const next = new Map(prev);
+        if (args.url === null) {
+          // create
+          const id = customId.toString();
+          setCustomId((id) => id + 1);
+          next.set(id, {
+            ...args.data,
+            url: CUSTOM_ID_PREFIX + id,
+            category: "people",
+          });
+        } else if (args.data === null) {
+          // delete
+          next.set(args.url, null);
+        } else {
+          // update
+          next.set(args.url, { ...prev.get(args.url)!, ...args.data });
+        }
+        return next;
+      });
+    },
+    [customId],
+  );
 
-  const updateResource = useCallback((resource: Resource<C>) => {
-    selectResource(resource);
-  }, []);
-  const createResource = useCallback(() => {
-    selectResource("new");
-  }, []);
   const localData = useMemo(() => {
-    return (query.data?.results ?? [])
+    return [
+      ...(query.data?.results ?? []),
+      ...[...resourceChanges.values()]
+        .filter((item) => !!item)
+        .filter((item) => isCustomId(item.url)),
+    ]
       .map((resource) => {
-        const savedResource = resourceChanges.get(resource.url);
-        return savedResource !== undefined ? savedResource : resource;
+        const savedResourceChanges = resourceChanges.get(resource.url);
+        function resolveResourceChanges() {
+          switch (savedResourceChanges) {
+            case undefined: // no changes
+              return resource;
+            case null: // deleted
+              return null;
+            default: // updated
+              return { ...resource, ...savedResourceChanges };
+          }
+        }
+        const newValue = resolveResourceChanges();
+        return newValue;
       })
       .filter((resource) => !!resource);
   }, [resourceChanges, query.data?.results]);
+
+  const newQuery = useMemo(() => {
+    return {
+      status: query.status,
+      data: localData,
+      error: query.error,
+    };
+  }, [localData, query.error, query.status]);
+
+  return {
+    query: newQuery,
+    save: saveResource,
+  };
 }
