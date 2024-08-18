f085e60d87891cc5f691ac38cf4b3b5826eae5af
yishai <yishai.pro@gmail.com>
2024-08-18 10:13:49 +0300

    for submission

--- src/components/PeopleDialog/PeopleDialog.tsx
+++ src/components/PeopleDialog/PeopleDialog.tsx
@@ -1,17 +1,21 @@
-import { ComponentProps, useMemo } from "react";
+import { ComponentProps, forwardRef, useCallback, useMemo } from "react";
 import AppDialog from "../dialog/Dialog";
-import { Fieldset, Field, Label, Input } from "@headlessui/react";
+import { Fieldset, Field, Label } from "@headlessui/react";
 import { Resource } from "../../api/api";
 import clsx from "clsx";
 import { Button } from "../button/Button";
+import { useForm } from "react-hook-form";
+
+type FormData = Resource<"people">;
 
 export function PeopleDialog({
   person,
-  onSubmit,
+  onSubmit: _onSubmit,
+  onClose,
   ...props
 }: Omit<ComponentProps<typeof AppDialog>, "title" | "children"> & {
-  person: "new" | Resource<"people"> | null;
-  onSubmit: (person: Resource<"people">) => void;
+  person: "new" | FormData | null;
+  onSubmit: (person: FormData) => void;
 }) {
   const title = useMemo(() => {
     if (!person) {
@@ -23,33 +27,48 @@ export function PeopleDialog({
     return `Edit person: ${person.name}`;
   }, [person]);
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    ...(person === "new" || person === null
+      ? {}
+      : { defaultValues: { name: "Joe" } }),
+  });
+  const onSubmit = useCallback(
+    (data: FormData) => {
+      _onSubmit(data);
+      onClose(false);
+    },
+    [_onSubmit, onClose],
+  );
+
   return (
-    <AppDialog {...props} open={!!person} title={title}>
+    <AppDialog {...props} onClose={onClose} open={!!person} title={title}>
       <Fieldset
         as="form"
         className="space-y-6 rounded-xl bg-white/5 p-6 sm:p-10"
-        onSubmit={(e) => {
-          e.preventDefault();
-          onSubmit(e.currentTarget);
-          props.onClose(false);
-        }}
+        onSubmit={handleSubmit(onSubmit)}
       >
         <Field>
           <DialogLabel> Name</DialogLabel>
-          <DialogInput name="name" autoFocus />
+          <DialogInput {...register("name")} autoFocus />
         </Field>
         <Field>
           <DialogLabel> Height </DialogLabel>
-          <DialogInput name="height" type="number" />
+          <DialogInput type="number" {...register("height")} />
         </Field>
         <Field>
           <DialogLabel> Gender </DialogLabel>
-          <DialogInput name="gender" type="text" />
+          <DialogInput {...register("gender")} />
         </Field>
 
+        {errors && JSON.stringify(errors, null, 2)}
+
         <div className="flex justify-between">
-          <Button onClick={() => props.onClose(true)}>Cancel</Button>
-          <Button type="submit">Order now</Button>
+          <Button onClick={() => onClose(false)}>Cancel</Button>
+          <Button type="submit">Submit</Button>
         </div>
       </Fieldset>
     </AppDialog>
@@ -62,12 +81,13 @@ function DialogLabel(props: { children: React.ReactNode }) {
   );
 }
 
-function DialogInput({
-  className,
-  ...props
-}: ComponentProps<typeof Input> & JSX.IntrinsicElements["input"]) {
+const DialogInput = forwardRef<
+  HTMLInputElement,
+  JSX.IntrinsicElements["input"]
+>(({ className, ...props }, ref) => {
   return (
-    <Input
+    <input
+      ref={ref}
       {...props}
       className={clsx(
         "mt-3 block w-full rounded-lg border-none bg-white/5 py-1.5 px-3 text-sm/6 text-white",
@@ -76,4 +96,4 @@ function DialogInput({
       )}
     />
   );
-}
+});
